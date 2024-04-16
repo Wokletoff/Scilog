44bd630ebbd28014b57251cc25c59760cff4428b
WokletOFF <telkov00>
2024-04-16 20:06:49 +0300

    feat: add base articles and decoding file

--- decoding.py
+++ decoding.py
@@ -1,7 +1,14 @@
 from base64 import b64decode
+import base64
+import zlib
+import urllib
 
-	
-file = open('/home/student/Документы/test/5000.tab', 'r')
-for line in file:
-    print(line)
-file.close()
+txt = open('/home/student/PycharmProjects/MagProject/project3/ProjectOnTeam/nlp/question_answering/5000.tab', 'r')
+while True:
+    line = txt.readline()
+    txtId = line.split("\t")[0]
+    src = line.split("\t")[1].replace("\n","")
+    text2 = base64.b64decode(src)
+    decompressed_data=zlib.decompress(text2, 16+zlib.MAX_WBITS)
+    print(src, decompressed_data)
+txt.close	
